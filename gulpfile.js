ad6b969c3ed8f09dff000f5afcd77fd17d3571c0
brianmayo <brianmay.dev@gmail.com>
2016-10-12 12:24:55 -0300

    gulp tasks. build package and bundle with browserify

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+const gulp = require('gulp')
+const browserify = require('browserify')
+const source = require('vinyl-source-stream')
+const babel = require('gulp-babel')
+
+gulp.task('bundle', () => {
+	return browserify("./src/html-logger.js")
+        .transform("babelify", {presets: ["es2015"]})
+        .bundle()
+        .pipe(source("html-logger.bundle.js"))
+        .pipe(gulp.dest('dist'));
+})
+
+gulp.task('build', () => {
+		return gulp.src("src/html-logger.js")
+		.pipe(babel({ presets: ["es2015"]}))
+		.pipe(gulp.dest("dist"))
+})
+
+gulp.task('default', ['build', 'bundle'])
